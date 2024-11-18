9b24db760de846e1e8a49462d7ebf706c8126807
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-18 10:35:50 +0100

    comit

--- 02_str_list_difference.py
+++ 02_str_list_difference.py
@@ -2,5 +2,10 @@
 
 # Elemeik (a karakterek) nem módosíthatóak!
 # Ez hibát okoz:
-sztring = 'Ismered ezt a számot?'
-sztring[0] = 'i'
+# sztring = 'Ismered ezt a számot?'
+# sztring[0] = 'i'
+
+list = ['I', 's', 'm', 'e', 'r', 'e', 'd', 'e', 'z', 't', 'a', 's', 'z', 'á', 'm', 'o', 't']
+print(list)
+list[0] = 'i'
+print(list)
